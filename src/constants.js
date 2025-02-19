24f05912d3c266a6767d627cf9cb18b45a1e1541
Chirag <chiragraj810@gmail.com>
2025-02-19 14:56:16 +0530

    update

---
+++ src/constants.js
@@ -0,0 +1,50 @@
+
+export const services = [
+    {
+        title: "Technology Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "App Developer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Photographer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    
+];
+
+export const name = 'Chirag';
+
+export const experiences = [
+    {
+        'company': 'Suvidha Mahila Foundation',
+        'role': 'Web Developer Intern',
+        'duration': 'Jan 2024 - Present',
+        'logo': 'https://miro.medium.com/v2/resize:fit:1100/format:webp/0*EInkHd4SsVXcr7Nk.jpg',
+        'points': [
+            'Spearheaded the design and development of the organization’s website, improving frontend and backend performance,which resulted in a 25% increase in user engagement and a 30% reduction in load time.📈',
+            'Enhanced and optimized key website features, improving functionality and user experience, contributing to a 40% increase in traffic within the first two months.⚙️',
+            
+        ],
+        'url': 'https://suvidhafoundationedutech.org/',
+    },
+    {
+        'company': 'Earth Root Foundation',
+        'role': 'Content Management Intern',
+        'duration': 'Aug 2023 - Oct 2023',
+        'logo': 'https://media.licdn.com/dms/image/v2/C4E0BAQFpvfjHyFO50w/company-logo_200_200/company-logo_200_200/0/1630622222730?e=2147483647&v=beta&t=dDa33W_BO7pEl7Bvv9FIVt5f5WNYtXmFJJroAyDg3Ik',
+        'points': [
+            'Assisted in organizing and executing environmental awareness campaigns focused on sustainability and waste management, reaching over 1,000 participants across multiple locations.',
+            'Conducted research and reporting on key environmental conservation efforts, contributing to data-driven initiatives that led to a 15% increase in public engagement.📱.'
+        ],
+        'url': 'https://earthrootfoundation.org/',
+    },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = process.env.REACT_APP_EMAIL_JS_SERVICE_ID;
+export const EMAIL_JS_TEMPLATE_ID = process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID;
+export const EMAIL_JS_PUBLIC_KEY = process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY;
